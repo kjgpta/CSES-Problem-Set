9c7efcff9d3740354cf8639be12ee0d0571f52b1
thecoder911 <f20190212@pilani.bits-pilani.ac.in>
2021-07-08 16:29:00 +0530

    adding minimizing coins

---
+++ minimizing_coins.py
@@ -0,0 +1,32 @@
+# we have imported sys library to modify the recursion limit to 10000 
+import sys
+sys.setrecursionlimit(10000)
+
+def best_sum(target_sum, numbers):
+    memo = {}
+    def helper(target_sum, numbers):
+        if target_sum == 0:
+            return []
+        if target_sum in memo:
+            return memo[target_sum]
+        shortest_combination = None
+        for num in numbers:
+            remainder = target_sum - num
+            if remainder >= 0:
+                combination = helper(remainder, numbers)
+                if combination is not None:
+                    combination = combination + [num]
+                    if shortest_combination is None or len(combination) < len(
+                        shortest_combination
+                    ):
+                        shortest_combination = combination
+        memo[target_sum] = shortest_combination
+        return memo[target_sum]
+    return helper(target_sum, numbers)
+
+
+no_of_items,net_sum = map(int,input().split())
+list_of_numbers = list(map(int,input().split()))
+print(len(best_sum(net_sum,list_of_numbers)))
+
+
